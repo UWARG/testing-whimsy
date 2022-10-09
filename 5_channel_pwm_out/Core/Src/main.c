7e3f473346adbae4cb0e680ee3316370ce30d186
billylin609 <linyuchen830@gmail.com>
2022-10-09 13:12:46 -0400

    checked can generate correct pwm output

--- 5_channel_pwm_out/Core/Src/main.c
+++ 5_channel_pwm_out/Core/Src/main.c
@@ -132,20 +132,9 @@ int main(void)
   while (1)
   {
 
-  //pull down the cs line to init the communication
-	  HAL_GPIO_WritePin(GPIOA, GPIO_port, GPIO_PIN_RESET);
 
-	//send the config to adc and then get the return value
-	  HAL_SPI_TransmitReceive(&hspi1, data_transmit, data_recieve, 3, 200);
 
-	//pull up the cs line to stop the communication
-	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
-
-	//get the most significant two digits of data bit wise operation
-	  ADC_in = (data_recieve[1] & 0X11);
-	  ADC_in = (ADC_in << 8) | data_recieve[2];
-
-	  double pwm_output = (double)ADC_in / adc_max;//in range 0 to 1
+	  double pwm_output = 0.5;//in range 0 to 1
 	  pwm_output = (pwm_output * (DutyCycle_max - DutyCycle_min) + DutyCycle_min) * CounterPeriod;//get the value in range from duty cycle max - min and get the amount of counter to compare
 
 	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pwm_output);
@@ -158,6 +147,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	 c
   }
   /* USER CODE END 3 */
 }
@@ -184,8 +174,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 96;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -199,10 +189,10 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
